707ba4984e88b05da855ac01b2ed0572760d84cc
MasaKnd <masahiro.knd.ds@gmail.com>
2024-07-24 14:19:28 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1>React Env</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
